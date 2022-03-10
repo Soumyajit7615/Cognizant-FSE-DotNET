ee469207f8f73b3e700c861e231cdd78a01d07ea
Ramkumar <64207937+ramkumarm15@users.noreply.github.com>
2022-03-10 09:48:22 +0530

    Update leftRotation.cs

--- Stage 1/Data Structures and Algorithm/leftRotation.cs
+++ Stage 1/Data Structures and Algorithm/leftRotation.cs
@@ -1,3 +1,17 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
 class Result
 {
 
@@ -9,16 +23,33 @@ class Result
      *  1. INTEGER d
      *  2. INTEGER_ARRAY arr
      */
-
-    public static List<int> rotateLeft(int d, List<int> arr)
+     
+     
+    public static void rotateLeft(int d, List<int> arr)
     {
-        for(int i = 0; i<d; i++){
-            int temp = arr[0];
-            for(int j = 0; j < arr.Count - 1; j++){
-                arr[j] = arr[j + 1];
-            }
-            arr[arr.Count - 1] = temp;
-        }   
-        return arr;
+        for(int i = d; i < arr.Count; i++){
+            Console.Write($"{arr[i]} ");
+        }
+        for(int i = 0; i < d; i++){
+            Console.Write($"{arr[i]} ");
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.rotateLeft(d, arr);
+
     }
 }
